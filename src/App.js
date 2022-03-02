63ee19431b13c71654d0a9e92afa1be338d6c334
StefanyCandida <stefany.candida@gmail.com>
2022-03-01 23:02:47 -0300

    Inserir projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component}  from 'react';
+import lixo from './lixo.png'
+import "./App.css"
+export default class ToDo extends Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+state ={
+   tarefa:"",
+   lista :[]
 }
 
-export default App;
+handleChange = (e)=>{
+  this.setState({tarefa: e.target.value})
+}
+
+
+add = () =>{
+if(this.state.tarefa.length > 0){
+    this.setState({      
+        lista: this.state.lista.concat({
+             tarefa:"",
+            id: Date.now()
+          }),
+          tarefa:"",
+        })
+      }
+    }
+ remove =(id) =>{
+    this.setState({
+        lista:this.state.lista.filter((item)=>(
+            item.id !==id
+        ))
+       
+    })
+
+ }
+ 
+
+    render(){ 
+        return(                      
+            <div>
+                <h1>Lista de Compras</h1>
+             <input value={this.state.tarefa} onChange={this.handleChange} />   
+             <button onClick={this.add}>INSERIR </button>  
+               <ol>
+                 {this.state.lista.map((item) =>(
+                     <li>
+                         {item.tarefa}
+                         {item.id}
+                         <img onClick={() => this.remove(item.id)} src={lixo} alt="iconde de pá "/>
+                     </li>
+                     
+                 ))}
+                 
+             </ol>
+            </div>
+            
+        )
+    }
+}
